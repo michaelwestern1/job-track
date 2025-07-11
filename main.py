5942f4e9a037b3e857ad9eb44465e1910e4ea6e2
Michael Pachowski <mpachows@uwo.ca>
2025-07-10 21:35:57 -0400

    Move backend files to root directory

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-from . import models, database
+import models
+import database
 from pydantic import BaseModel
 
 models.Base.metadata.create_all(bind=database.engine)
