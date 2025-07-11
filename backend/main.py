3c90094e87a5a5690a9b606e5732f548ed21b5cd
Michael Pachowski <mpachows@uwo.ca>
2025-07-10 21:26:45 -0400

    Fix relative imports for Render

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-import models
-import database
+from backend import models, database
 from pydantic import BaseModel
 
 models.Base.metadata.create_all(bind=database.engine)
